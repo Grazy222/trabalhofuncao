a6da509834d7f87554db4f0b85a0bca2889b0c68
Grazy222 <graciele222@gmail.com>
2023-01-30 20:43:04 -0300

    finalizando trabalho

---
+++ script.js
@@ -0,0 +1,62 @@
+
+
+//1 - crie uma função que exiba uma mensagem no console
+    // let nome = `Olá,seja bem vindo!`
+     
+     //function exibir() {
+      // console.log (`${nome}`) 
+    //}
+    //exibir ()
+   
+
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+
+  //function mostrar(nome) {
+  //console.log (`meu nome é ${nome}`)
+    
+ //}
+ //mostrar ( "Grazy") 
+
+//3 - crie uma função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console
+  
+//let info = function (nome , idade , estilo){
+  // console.log(nome);
+    //  console.log(idade);
+      // console.log(estilo);
+      //console.log(` Meu nome é ${nome}, tenho ${idade} eu gosto de ${estilo} `)
+   //}
+  
+   //info(`Grazy`, " 39 anos", "Hip-hop")
+
+
+//4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+   
+  //let filme = function (filme, musica ){
+    //console.log(filme);
+     //console.log(musica);
+      //console.log(` Gosto desse filme de ${filme}, amo essa musica ${musica} `)
+  //}
+ 
+  //filme(`terror`,"left the go" )
+ 
+
+
+//5 - crie uma função que retorne o triplo do número recebido no parâmetro da função
+
+  //function triplo (numero2){
+     //    return numero2 * 3
+  //}
+   //console.log(triplo(20))
+  
+
+//6 - crie uma função que  verifique se uma  variável é true ou false
+//let fruta = "Manga"
+  
+ //switch (fruta) {
+   //  case "Abacaxi":
+     //    console.log("Tem certeza que é essa fruta?")
+       // break; 
+        //default: 
+         //console.log("true")
+
+//}
